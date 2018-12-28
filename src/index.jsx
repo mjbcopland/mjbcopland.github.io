e3766d18c3d795ee86225eb2cab91832c2dbaa58
Michael Copland <mjbcopland@gmail.com>
2018-12-28 22:21:57 +0000

    Hello, World!

---
+++ src/index.jsx
@@ -0,0 +1,8 @@
+/* global document */
+
+import React from 'react';
+import { render } from 'react-dom';
+
+const App = () => <div>Hello, World!</div>;
+
+render(<App />, document.getElementById('root'));
--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,23 @@
+const path = require('path');
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+
 module.exports = {
+  resolve: {
+    alias: { '~': path.resolve(__dirname, 'src/') },
+    extensions: ['.js', '.jsx', '.json'],
+  },
   module: {
     rules: [
       {
         test: /\.jsx?$/,
         exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-        },
+        use: ['babel-loader', 'eslint-loader'],
+      },
+      {
+        test: /\.html$/,
+        use: ['html-loader'],
       },
     ],
   },
+  plugins: [new HtmlWebPackPlugin({ template: 'src/index.html' })],
 };
